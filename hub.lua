14bd668b4df70d0cfa32d931ae99a1834208ff1f
zediki <122214068+zediki@users.noreply.github.com>
2024-05-09 12:59:09 +0300

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,1 +1,33 @@
+local UILib = loadstring(game:HttpGet('https://raw.githubusercontent.com/StepBroFurious/Script/main/HydraHubUi.lua'))()
 
+local Window = UILib.new("Toilet Monstor Hub", game.Players.LocalPlayer.Name, "User")
+
+local Category1 = Window:Category("Main", "http://www.roblox.com/asset/?id=8395621517")
+
+local SubButton1 = Category1:Button("Main2", "")
+
+local Section1 = SubButton1:Section("Section", "Left")
+
+
+local places = {
+  [10449761463] = true,
+}
+
+Section1:Button({
+
+    Title = "Detect Game",
+
+    ButtonName = "Detect the Game!!",
+
+    Description = "Loading script for this game",
+    }, function(value)
+
+    print(value)
+
+      if places[game.PlaceId] and places[game.PlaceId] == true then
+        print("https://raw.githubusercontent.com/zediki/Toilet-Montor-hub/main/".. game.PlaceId ..".lua")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/zediki/Toilet-Montor-hub/main/".. game.PlaceId ..".lua"))()
+      else
+        print("Unkown Game")
+      end
+end)
